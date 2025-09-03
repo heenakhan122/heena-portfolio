2fe0b17671b2bb59a855461ca1b3ca6a630dccf4
heenakhan122 <heenakhan121235@gmail.com>
2025-09-02 21:35:51 -0400

    Update vite.config.github.ts

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -13,9 +13,9 @@ export default defineConfig({
   root: "client",                       // your index.html is in client/
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+    "@": path.resolve(__dirname, "client/src"),
+    "@shared": path.resolve(__dirname, "shared"),
+    "@assets": path.resolve(__dirname, "client/src/assets"),
     },
   },
   build: {
