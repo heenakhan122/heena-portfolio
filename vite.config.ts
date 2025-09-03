76e96114f8b3ed860b00be11e06f9cfca6452412
heenakhan122 <heenakhan121235@gmail.com>
2025-09-02 21:42:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// vite.config.github.ts
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -8,18 +8,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/heena-portfolio/",            // must match repo name
+  base: "/heena-portfolio/",          // must match repo name
+  root: "client",                     // your index.html is in client/
   plugins: [react()],
-  root: "client",                       // your index.html is in client/
   resolve: {
     alias: {
-    "@": path.resolve(__dirname, "client/src"),
-    "@shared": path.resolve(__dirname, "shared"),
-    "@assets": path.resolve(__dirname, "client/src/assets"),
+      "@": path.resolve(__dirname, "client/src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "client/src/assets"), // images live here
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "dist"), // final static site
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
